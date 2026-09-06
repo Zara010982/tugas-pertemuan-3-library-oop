55f9ad32a1890d22ed36094a8208d56b70b4dc07
Zara010982 <azzahranursalsabila651@gmail.com>
2026-09-07 00:30:38 +0700

    Update index for DigitalBook
    Added support for digital books by including DigitalBook class and displaying its information.

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 
 require_once "Book.php";
 require_once "Member.php";
+require_once "DigitalBook.php";
 
 echo "<h1>Library OOP Mini</h1>";
 
@@ -15,10 +16,27 @@ $member1 = new Member(
     "2410631250045"
 );
 
+$digitalBook = new DigitalBook(
+    "Laravel untuk Pemula",
+    "Budi Santoso",
+    "PDF"
+);
+
 echo "<h2>Informasi Buku</h2>";
 echo $book1->getInfo();
 
+echo "<br><br>";
+
 echo "<h2>Informasi Member</h2>";
 echo $member1->getInfo();
+
+echo "<br><br>";
+
+echo "<h2>Informasi Digital Book</h2>";
+echo $digitalBook->getInfo();
+
+echo "<br><br>";
+
+echo $digitalBook->download();
 
 ?>
